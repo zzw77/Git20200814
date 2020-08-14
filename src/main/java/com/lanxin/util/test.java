260d7ac7fae2a9e6cd1b3293094315498e223d6d
zzw <zz@126.com>
2020-08-14 15:30:57 +0800

    修改

--- src/main/java/com/lanxin/util/test.java
+++ src/main/java/com/lanxin/util/test.java
@@ -1,5 +1,9 @@
 package com.lanxin.util;
+
+import javax.naming.Name;
+
 public class test {
+    private String name;
     public static void main(String[] args) {
         System.out.println("111111111111111111");
     }
